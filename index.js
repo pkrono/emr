5b6cd9ac641cc48a0d37665f0ca83c12c0ebaedc
Rono <peter@copiakenya.com>
2024-06-12 05:31:29 +0300

    CRUD endpoints created

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+const Joi = require('joi');
 const express = require('express');
-
 const app = express();
 
 app.use(express.json()); //parsing Json Objects
@@ -21,12 +21,12 @@ app.get('/api/drugs', (req, res) =>  {
 });
 
 //Get drug with given id
-app.get('/api/drugs/:id', (req, res) => { 
+app.get('/api/drugs/:id', (req, res) => {
 
     //res.send({ 'id': req.params.id });
     const drug = drugs.find(c => c.id === parseInt(req.params.id));
     if (!drug) {
-      res.status(404).send('drug with given id not found');
+        return res.status(404).send('drug with given id not found');
     }
     res.send(drug);
     //res.send(req.query);
@@ -34,6 +34,14 @@ app.get('/api/drugs/:id', (req, res) => {
 })
 
 app.post('/api/drugs', (req, res) => {
+
+    const result = validateDrug(req.body);
+
+    if (result.error) {
+        //http status code 400-Bad request
+        return res.status(400).send(result.error.details[0].message);
+    }
+
     const drug = {
         id: drugs.length + 1,
         name: req.body.name
@@ -42,8 +50,48 @@ app.post('/api/drugs', (req, res) => {
     drugs.push(drug);
     res.send(drug);
 });
-//app.put()
-//app.delete()
+
+app.put('/api/drugs/:id', (req, res) => {
+    //check if exist
+
+    const drug = drugs.find(c => c.id === parseInt(req.params.id));
+    if (!drug) { 
+        return res.status(404).send('Drug with the given id not found!');
+    }
+   
+    //Validate
+   const result = validateDrug(req.body)
+
+    if (result.error) {
+        return res.status(400).send(result.error.details[0].message);
+    }
+
+    //Update drug
+    drug.name = req.body.name;
+    res.send(drug);
+});
+
+app.delete('/api/drugs/:id', (req, res) => {
+    //check if exists
+    const drug = drugs.find(c => c.id === parseInt(req.params.id));
+    if (!drug) { 
+        return res.status(404).send('Drug with the given id not found!');
+    }
+
+    //Delete
+    const index = drugs.indexOf(drug);
+    drugs.splice(index, 1);
+
+    res.send(drug)
+});
+
+function validateDrug(drug) { 
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    return schema.validate(drug);
+}
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on port ${port}...`))
